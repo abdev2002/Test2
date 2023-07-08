20be13d1dff7fe47edc6ed082c1944db659be4b1
abdev2002 <abd.mghl.dev@gmail.com>
2023-07-08 19:08:53 +0500

    I have just changed the index file and updated others

--- Index/Index.js
+++ Index/Index.js
@@ -1,21 +1,6 @@
-// function wait(ms){
-//     var start = new Date().getTime();
-//     var end = start;
-//     while(end < start + ms) {
-//       end = new Date().getTime();
-//    }
-//  }
-
-//  var width = 270;
-//  var pre = 270;
-//  for (let i = 0; i < 10; i++) {
-//     // const element = array[i];
-//     // while (width > 0) {
-//         wait(500)
-//         width = document.getElementById("BOX").offsetWidth;
-//         if (width< pre){
-//             console.log(width)
-//         }
-//         pre = width;
-// //  }
-// }
+// Wait for the animation to complete
+const lineElement = document.querySelector(".line");
+lineElement.addEventListener("animationend", () => {
+  // Open a new window or perform any desired action here
+  window.location = "/Login/login.html";
+});
